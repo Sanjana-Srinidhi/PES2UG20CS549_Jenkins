3ab564d5562a6cb001984c2f4acef10d67b1d7a1
Sanjana-Srinidhi <sanjanasrinidhi1810@gmail.com>
2023-02-15 20:02:33 +0530

    Second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World, from PES2UG20CS549\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "We did it!!!!";
